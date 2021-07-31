03cbbd19055cb92ea8b6fb571eeea5450b76f510
Sai Reddy <sai6221@gmail.com>
2021-07-31 14:59:33 +0530

    1st View Done

--- BMI_Calc/ViewController.swift
+++ BMI_Calc/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var weightSlide: UISlider!
+    @IBOutlet weak var heightSlide: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightLabel.text = "0m"
+        weightLabel.text = "0kg"
     }
 
-
+    @IBAction func heightSlider(_ sender: UISlider) {
+        heightLabel.text = String(format:"%0.2f",sender.value)+"m"
+    }
+    
+    @IBAction func weightSilder(_ sender: UISlider) {
+        weightLabel.text = String(format:"%0.2f",sender.value)+"kg"
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print( weightSlide.value / pow(heightSlide.value,2))
+    }
+    
 }
 
